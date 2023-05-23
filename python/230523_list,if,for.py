1b138269823b56dcb978192587b6627c76069a72
kyuriiii <kyuri99@naver.com>
2023-05-23 19:28:42 +0900

    feat: Start second lesson

---
+++ python/230523_list,if,for.py
@@ -0,0 +1,17 @@
+'''
+# 02_다양한 자료형 실습1
+rainbow=['red','orange','yellow','green','blue','indigo','purple']
+
+print('(1) ',end='')
+print(rainbow[2])
+print('(2) ',end='')
+rainbow.sort()
+print(rainbow)
+print('(3) ',end='')
+rainbow.append('pink')
+print(rainbow)
+print('(4) ',end='')
+del rainbow[3:]
+print(rainbow)
+'''
+
